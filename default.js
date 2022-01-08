50ec1c0833f61ecb70ed1c646c8612059a061106
Meilyn22 <samsonidoko3@byui.edu>
2022-01-08 16:38:55 -0700

    added css to page

---
+++ default.js
@@ -0,0 +1,12 @@
+let date = document.querySelector('#date')
+let copy = document.querySelector('#copy')
+copy.textContent = '2021 .:|:. Samson O. Idoko .:|:. Idaho'
+let options =  {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour : "2-digit",
+    minute : "2-digit",
+    second:"2-digit" 
+}
+date.textContent = 'Last updated: ' + new Date(document.lastModified).toLocaleString('en-US', options).replace(",", " ")
