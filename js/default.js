7e92b01b86b8a056f321ec492652b117c7a6cd85
Meilyn22 <samsonidoko3@byui.edu>
2022-01-11 10:33:03 -0700

    Corrected contrast, improved javascript

--- js/default.js
+++ js/default.js
@@ -1,12 +1,5 @@
-let date = document.querySelector('#date')
-let copy = document.querySelector('#copy')
-copy.textContent = '2022 .:|:. Samson O. Idoko .:|:. Idaho'
-let options =  {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour : "2-digit",
-    minute : "2-digit",
-    second:"2-digit" 
-}
-date.textContent = 'Last updated: ' + new Date(document.lastModified).toLocaleString('en-US', options).replace(",", " ")
+let modifiedDate = document.querySelector('#date')
+let copy = document.querySelector('#copyright')
+
+modifiedDate.textContent = new Date(document.lastModified)
+copy.textContent = new Date().getFullYear()
