ccc8555a6484a176e10f3d98ac84bca3ffb8a645
Meilyn22 <samsonidoko3@byui.edu>
2022-01-30 19:31:39 -0700

    added grid

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -5,3 +5,9 @@ function toggleMenu() {
 
 const x =  document.querySelector("#hamburger");
 x.onclick = toggleMenu;
+
+let modifiedDate = document.querySelector('#date')
+let copy = document.querySelector('#copyright')
+
+modifiedDate.innerHTML = (document.lastModified)
+copy.textContent = new Date().getFullYear()
